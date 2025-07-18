682690b7c29e3be22b4540b640d728b8dcfb194d
LucasPontees <eulpontes@gmail.com>
2025-07-18 18:50:07 -0400

    feat: enhance user DTOs with detailed ApiProperty descriptions and examples for better API documentation

--- src/users/dtos/users.user.dto.ts
+++ src/users/dtos/users.user.dto.ts
@@ -2,26 +2,44 @@ import { IsNotEmpty } from "class-validator";
 
 import { ApiProperty } from "@nestjs/swagger";
 export class LoginUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: "The login of the user",
+    example: "john.doe",
+  })
   @IsNotEmpty()
   readonly login: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: "The password of the user",
+    example: "password",
+  })
   @IsNotEmpty()
   readonly password: string;
 }
 export class CreateUserDto {
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: "The name of the user",
+    example: "John",
+  })
   name: string;
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: "The surname of the user",
+    example: "Doe",
+  })
   surname: string;
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: "The login of the user",
+    example: "john.doe",
+  })
   login: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: "The password of the user",
+    example: "password",
+  })
   @IsNotEmpty()
   password: string;
 }
